be369c82457da8c97dfe77f7e02c05d0869e9f46
thoanv <vanthoa225@gmail.com>
2023-01-03 16:59:45 +0700

    up code

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -47,7 +47,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-white" href="{{route('reward')}}">
+                <a class="nav-link text-white" href="{{route('reward')}}" target="_blank">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">auto_mode</i>
                     </div><span class="nav-link-text ms-1">Thực hiện quay</span>
