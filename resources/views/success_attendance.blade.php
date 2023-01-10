a569169ab6efda3d649aef76d8b58aa6cfae9973
thoanv <vanthoa225@gmail.com>
2023-01-10 11:30:15 +0700

    up code

--- resources/views/success_attendance.blade.php
+++ resources/views/success_attendance.blade.php
@@ -3,9 +3,13 @@
 @section('content')
     <div class="container my-auto">
                 <div class="row">
-                    <div class="col-lg-4 col-md-8 col-12 mx-auto">
+                    <div class="col-lg-4 col-md-8 col-12 mx-auto mt-mobile">
                         <div class="card z-index-0 fadeIn3 fadeInBottom">
-                            @include('components.header', ['title' => 'Đăng ký thành công', 'about' => \App\Models\Information::find(1)])
+                            <div class="card-header p-0 position-relative mt-n5 mx-3 z-index-2">
+                                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1 text-center">
+                                    <h4 class="text-white font-weight-bolder text-center mt-2 mb-0 text-uppercase">Đăng ký thành công</h4>
+                                </div>
+                            </div>
                             <div class="pyro"><div class="before"></div><div class="after"></div></div>
                             <div class="card-body attendance">
                                 <div class="box-result">
