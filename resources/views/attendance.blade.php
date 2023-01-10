3e00e6c0611acf9e8716a8ae27472109402d0bb2
thoanv <vanthoa225@gmail.com>
2023-01-10 11:35:12 +0700

    up cdoe

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-lg-4 col-md-8 col-12 mx-auto mt-mobile">
                 <div class="card z-index-0 fadeIn3 fadeInBottom">
-                    <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                    <div class="card-header p-0 position-relative mt-n5 mx-3 z-index-2">
                         <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1 text-center">
                             <h4 class="text-white font-weight-bolder text-center mb-0">CHECK IN</h4>
                         </div>
